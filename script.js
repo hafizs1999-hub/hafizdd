9d32a9a526fb5a1bc20ca2526910c5392577d8b0
hafizs1999-hub <hafizs1999@gmail.com>
2026-06-18 13:53:12 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+document.querySelector(".apply-btn").addEventListener("click", function(){
+    alert("Application Form Coming Soon!");
+});
+
+document.querySelector(".course-btn").addEventListener("click", function(){
+    alert("Explore Our Courses");
+});
+
+document.querySelector(".about-btn").addEventListener("click", function(){
+    alert("Welcome to E-Learning & Earning Ltd");
+});
